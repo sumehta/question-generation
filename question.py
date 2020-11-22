18821d9106a4a7c8205abbfeb79cab3d2e17f82f
Sneha Mehta <snehamehta@vt.edu>
2020-11-21 20:07:18 -0500

    Update question.py

--- question.py
+++ question.py
@@ -5,8 +5,8 @@ from question_generator import QuestionGenerator
 
 def add_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '-sentence', type=str, help="The sentence for which questions are to be generated.")
-    parser.add_argument('-t', '-question_type', type=str, default=['Wh', 'Are', 'Who', 'Do'], choices=['Wh', 'Are', 'Who', 'Do', 'All'], help='The types of questions to be generated.')
+    parser.add_argument('-s', '--sentence', type=str, help="The sentence for which questions are to be generated.")
+    parser.add_argument('-t', '--question_type', type=str, default=['Wh', 'Are', 'Who', 'Do'], choices=['Wh', 'Are', 'Who', 'Do', 'All'], help='The types of questions to be generated.')
     return parser.parse_args()
 
 
